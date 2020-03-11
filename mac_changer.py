16aca8e6aa1d3e91770cff479ad8c87ac5592d48
nando-29 <62020794+nando-29@users.noreply.github.com>
2020-03-10 21:12:20 -0400

    Add files via upload

---
+++ mac_changer.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+
+import subprocess
+import optparse
+import re
+
+def get_arguments():
+    nando = optparse.OptionParser()
+    nando.add_option("-i", "--interface", dest="interface", help="Choose interface")
+    nando.add_option("-m", "--mac_adress", dest="mac_change", help="Choose your mac adress")
+    (options,argumuments) = nando.parse_args()
+    if not options.interface:
+        nando.error("Input your interface properly")
+    if not options.mac_change:
+        nando.error("Coludn't find Mac Adress")
+    return options
+
+def mac_change(interface,mac_change):
+    subprocess.call(["ifconfig", interface, "down"])
+    subprocess.call(["ifconfig", interface, "hw", "ether", mac_change])
+    subprocess.call(["ifconfig", interface, "up"])
+
+def get_current_mac(interface):
+    ifconfig_result = subprocess.check_output(["ifconfig", interface])
+    mac_result = re.search(r"\w\w:\w\w:\w\w:\w\w:\w\w:\w\w", ifconfig_result)
+
+    if mac_result:
+        return mac_result.group(0)
+    else:
+        print("[-] Could not read MAC address")
+
+
+options = get_arguments()
+
+current_mac = get_current_mac(options.interface)
+print("Current MAC = " + str(current_mac))
+
+mac_change(options.interface, options.mac_change)
+
+current_mac = get_current_mac(options.interface)
+if current_mac == options.mac_change:
+    print("[+] Mac address was succesfully changed to " + current_mac)
+else:
+    print("[-] Mac address didn't get change")
+
